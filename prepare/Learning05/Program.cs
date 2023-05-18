9b2120a6bca2b92c4f38786d1f0ff81939ad5581
Miranda Saenz <sae22006@byui.edu>
2023-05-18 16:47:57 -0700

    Completed Prepare week05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shape = new List<Shape>();
+        
+        Square square = new Square("purple", 3);
+        shape.Add(square);
+
+        Rectangle rectangle = new Rectangle("teal", 3, 4);
+        shape.Add(rectangle);
+
+        Circle circle = new Circle("red", 5);
+        shape.Add(circle);
+
+        foreach (Shape i in shape)
+        {
+            string color = i.GetColor();
+            double area = i.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}");
+        }
     }
 }
---
+++ prepare/Learning05/Shape.cs
@@ -0,0 +1,22 @@
+
+public abstract class Shape
+{
+    protected string _color;
+
+    public Shape(string color)
+    {
+        _color = color;
+    }
+
+    public string GetColor()
+    {
+        return _color;
+    }
+
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+
+    public abstract double GetArea();
+}
